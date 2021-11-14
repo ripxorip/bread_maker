e668c68a220e69d3b0843c311c05d337ba4992ed
Philip K. Gisslow <ripxorip@gmail.com>
2021-11-14 16:34:22 +0000

    Inc

---
+++ main.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, Response, request
+import io
+import picamera
+from threading import Condition
+
+class StreamingOutput(object):
+    def __init__(self):
+        self.frame = None
+        self.buffer = io.BytesIO()
+        self.condition = Condition()
+
+    def write(self, buf):
+        if buf.startswith(b'\xff\xd8'):
+            # New frame, copy the existing buffer's content and notify all
+            # clients it's available
+            self.buffer.truncate()
+            with self.condition:
+                self.frame = self.buffer.getvalue()
+                self.condition.notify_all()
+            self.buffer.seek(0)
+        return self.buffer.write(buf)
+
+camera = picamera.PiCamera(resolution='640x480', framerate=24)
+output = StreamingOutput()
+#Uncomment the next line to change your Pi's Camera rotation (in degrees)
+#camera.rotation = 90
+camera.start_recording(output, format='mjpeg')
+
+# App Globals (do not edit)
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html') #you can customze index.html here
+
+def gen(output):
+    #get camera frame
+    while True:
+        with output.condition:
+            output.condition.wait()
+            frame = output.frame
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(output),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', debug=False)
--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 import board
 import neopixel
+from w1thermsensor import W1ThermSensor
 
 pixels = neopixel.NeoPixel(board.D18, 30)
 
@@ -9,4 +10,9 @@ pixels.fill((255, 255, 180))
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)
-GPIO.output(17, 0)
+GPIO.output(17, 1)
+
+
+sensor = W1ThermSensor()
+temperature = sensor.get_temperature()
+print("The temperature is %s celsius" % temperature)
